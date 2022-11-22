de654aaa35db607a8d5d2b4a9a310c4c3fd5cef8
Wolfmatee <da.mallma@duocuc.cl>
2022-11-21 22:58:56 -0300

    mensaje

--- PuntoCompraJava/src/modelo/Compra.java
+++ PuntoCompraJava/src/modelo/Compra.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
+/*hola git*/
 package modelo;
 
 import java.time.LocalDate;
